febe2936e1eb0e3a6e7a60042cb184bea0078e49
Erica Hashert <ericahashert@gmail.com>
2022-09-21 21:07:12 -0600

    commit tree adce2f9f4a5aaf78ea4b359e1664c36e54233940

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
-# Make your shoe class here!
+class Shoe 
+    attr_reader :brand
+    attr_accessor :color
+    attr_reader :size
+    attr_accessor :material
+    attr_accessor :condition
+
+
+    def initialize (brand)
+        @brand = brand
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
